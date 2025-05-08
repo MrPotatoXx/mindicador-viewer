0523b01f9b01eedbb858a0921dec2556b3acc3a3
MrPotatoXx <salinasgiovanni25@gmail.com>
2025-05-07 23:42:14 -0400

    Agregar manejo de errores en las funciones de obtención de indicadores desde la API de Mindicador

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,17 @@
 export async function getIndicadores() {
-	const res = await fetch('https://mindicador.cl/api');
-	return res.json();
+	const res = await fetch("https://mindicador.cl/api");
+	if (!res.ok) throw new Error("Error al obtener los indicadores");
+	return await res.json();
   }
-  
+
   export async function getIndicador(indicador) {
 	const res = await fetch(`https://mindicador.cl/api/${indicador}`);
-	return res.json();
+	if (!res.ok) throw new Error("Error al obtener el indicador");
+	return await res.json();
   }
-  
-  export async function getIndicadoresPorFecha(fecha) {
-	const res = await fetch(`https://mindicador.cl/api/${fecha}`);
-	return res.json();
-  }
-  
+
+  export async function getIndicadorPorFecha(indicador, fecha) {
+  const res = await fetch(`https://mindicador.cl/api/${indicador}/${fecha}`);
+  if (!res.ok) throw new Error("Error al obtener el indicador");
+  return await res.json();
+}
